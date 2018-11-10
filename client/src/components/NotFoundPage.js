b535be7a5b73ec91018188ad2938324b8f06c4f4
ptippa <pranav.tippa@gmail.com>
2018-11-10 17:23:34 -0500

    404 Error P2

--- client/src/components/NotFoundPage.js
+++ client/src/components/NotFoundPage.js
@@ -1,17 +1,14 @@
 import React from "react";
-import galaxy from "../images/Galaxy.jpg";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
   <div className={"header"}>
     <h1>404 ERROR</h1>
     <h2>The server could not find the URL you requested.</h2>
 
-    <div className={"nav"}>
-      <a href="Home.html">Return to Home Page</a>
-      <br />
-    </div>
-
-    <img src={galaxy} alt="Background" />
+    <Link to="/" className={"link"}>
+      Return to Home Page
+    </Link>
   </div>
 );
 
