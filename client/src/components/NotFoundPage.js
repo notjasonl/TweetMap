8ded05e88af837bb0ac61346c0eded4536b2580c
ptippa <pranav.tippa@gmail.com>
2018-11-10 16:45:22 -0500

    404 Error P1

--- client/src/components/NotFoundPage.js
+++ client/src/components/NotFoundPage.js
@@ -1,5 +1,18 @@
 import React from "react";
+import galaxy from "../images/Galaxy.jpg";
 
-const NotFoundPage = () => <div>404</div>;
+const NotFoundPage = () => (
+  <div className={"header"}>
+    <h1>404 ERROR</h1>
+    <h2>The server could not find the URL you requested.</h2>
+
+    <div className={"nav"}>
+      <a href="Home.html">Return to Home Page</a>
+      <br />
+    </div>
+
+    <img src={galaxy} alt="Background" />
+  </div>
+);
 
 export default NotFoundPage;
