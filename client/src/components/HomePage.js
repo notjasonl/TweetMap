4ad308f22abebd7436376885bc45ecc0dccd64e0
dumblole <dumblole@gmail.com>
2018-11-10 19:46:46 -0500

    home

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -4,24 +4,18 @@ import logo from '../logo.svg';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-         <form action="">
-            <input type="text" name="name" placeholder="#"/>
-            </form>
+      <div className="HomeBack">
+      <div className={"HomeScreen"}>
+      <form action="" className="HomeForm">
+            <input type="text" name="hashtag" placeholder="#hashtag"/>
+      </form></div>
+      
+      <h1>YOTE</h1>
+      <h1>YOTE</h1>
+      <h1>YOTE</h1>
+      <h1>YOTE</h1>
+      <h1>YOTE</h1>
+
       </div>
     );
   }
