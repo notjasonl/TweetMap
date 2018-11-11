0ed01556556f5deee536b8485fd083edfead3d83
dumblole <dumblole@gmail.com>
2018-11-10 23:46:05 -0500

    home

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import logo from '../images/TweetMapPointer.png';
 
 class App extends Component {
   render() {
     return (
       <div className="HomeBack">
       <div className={"HomeScreen"}>
+      <div className="FlexRow HeaderTitle">
+       <img src={logo} alt="Smiley face" className={"LogoImg"}/> 
+       <h1 className="HeaderHead bluetext">{" "}Tweet</h1>
+       <h1 className="HeaderHead">Map</h1>
+       </div>
       <form action="" className="HomeForm">
-            <input type="text" name="hashtag" placeholder="#hashtag"/>
+      <div className={"FormFlex"}>
+      <div>
+            <label className={"HomeText"}>#</label><input type="text" name=" tag" placeholder="tag" className={"HomeInput"}/>
+           </div> 
+           <div>
+            <input type="submit" value="Submit" className="FormButt"/></div></div>
       </form></div>
       
       <h1>YOTE</h1>
