a8b36ce358b65b8b291b7bb34e08698e8b17b44d
Brian X <brianx0011@gmail.com>
2018-11-11 06:59:41 -0500

    Added map

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Router, Route, Switch, Link, NavLink } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
+
 import NotFoundPage from "../components/NotFoundPage";
 import HomePage from "../components/HomePage"
+import MapPage from "../components/MapPage"
 
 const history = createHistory();
 
@@ -11,6 +13,7 @@ const AppRouter = () => (
     <div>
       <Switch>
         <Route path="/" component={HomePage} exact={true} />
+        <Route path="/map" component={MapPage} exact={true} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
